9416d379d197ca0c340c70f481756cd53b1dd832
Ephraim Kandov <efraimkandov@gmail.com>
2024-03-29 18:15:03 -0400

    updated numbers_to_words.py

--- numbers_to_words.py
+++ numbers_to_words.py
@@ -1,25 +1,23 @@
-def main:
+def main():
     userInput = input("Please enter an integer: ")
     checkInput(userInput)
-    pass
 
 def checkInput(userInput):
     numDict = {
         "0": "Zero", "1": "One", "2": "Two", "3": "Three", "4": "Four",
-        "5": "Five", "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine"
+        "5": "Five", "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine",
+        "10": "Ten", "11": "Eleven", "12": "Twelve", "13": "Thirteen",
+        "14": "Fourteen", "15": "Fifteen", "16": "Sixteen", "17": "Seventeen",
+        "18": "Eighteen", "19": "Nineteen"
     }
 
-    tenList = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", 
-               "Fifteen", "Nineteen", "Twenty", "Thirty", "Fourty", 
-               "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+    tenList = ["Twenty", "Thirty", "Fourty", "Fifty", 
+               "Sixty", "Seventy", "Eighty", "Ninety"]
 
     cardinals = ["Hundred", "Thousand", "Million", "Billion", "Trillion"]
-
-    match userInput:
-        case userInput == 0:
-            return print(f"{numDict["0"]}")
-        case userInput >= 1 and userInput < 10:
-            pass
-    pass
+    if float(userInput) < 20:
+        return print(f"{numDict[userInput]}")
+    if float(userInput) >= 20 and float(userInput) < 100:
+        pass
 
 main()
