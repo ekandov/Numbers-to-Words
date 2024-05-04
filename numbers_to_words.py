9bb77611407177f943adda38b35742cc181312a5
Ephraim Kandov <efraimkandov@gmail.com>
2024-05-04 13:21:17 -0400

    Updated numbers_to_words.py

--- numbers_to_words.py
+++ numbers_to_words.py
@@ -1,16 +1,33 @@
+numDict = {
+    1: "One", 2: "Two", 3: "Three", 4, "Four",
+    5: "Five", 6: "Six", 7: "Seven", 8, "Eight", 9: "Nine",
+    10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen",
+    14: "Fourteen", 15: "Fifteen", 16: "Sixteen", 17: "Seventeen",
+    18: "Eighteen", 19: "Nineteen", 20: "Twenty", 30: "Thirty",
+    40: "Fourty", 50: "Fifty", 60: "Sixty", 70: "Seventy",
+    80: "Eighty", 90: "Ninety", 100: "Hundred", 1000: "Thousand",
+    1000000: "Million", 10000000: "Billion", 100000000: "Trillion"
+}
+
 def main():
-    userInput = input("Please enter an integer: ")
+    userInput = input("Please type a number. ")
+    print(int(userInput))
     checkInput(userInput)
 
 def checkInput(userInput):
-    numDict = {
-        "0": "Zero", "1": "One", "2": "Two", "3": "Three", "4": "Four",
-        "5": "Five", "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine",
-        "10": "Ten", "11": "Eleven", "12": "Twelve", "13": "Thirteen",
-        "14": "Fourteen", "15": "Fifteen", "16": "Sixteen", "17": "Seventeen",
-        "18": "Eighteen", "19": "Nineteen"
-    } 
-    
+    resultInput = convert(int(userInput))
+    return print(resultInput)
+
+def convert(userInput):
+    result = ""
+    for divisor, letters in numDict:
+        result += userInput * letters
+        userInput %= divisor
+        print(userInput)
+        return result
+
+
+"""
     tenCard = {"2": "Twenty", "3": "Thirty", "4": "Fourty","5": "Fifty", 
                "6": "Sixty","7": "Seventy","8": "Eighty","9": "Ninety"}
 
@@ -18,7 +35,7 @@ def checkInput(userInput):
 
     if float(userInput) < 20:
         return print(f"{numDict[userInput]}")
-
+"""
 
 
 """
