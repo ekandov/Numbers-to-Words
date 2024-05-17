edabaf50752713866af4ca1ad242cac0d161ff74
Ephraim Kandov <efraimkandov@gmail.com>
2024-05-16 23:21:28 -0400

    Updated numbers_to_words.py

--- numbers_to_words.py
+++ numbers_to_words.py
@@ -11,8 +11,11 @@ numDict = {
 cardDict = {
     0: "", 20: "Twenty", 30: "Thirty",40: "Fourty", 50: "Fifty", 
     60: "Sixty", 70: "Seventy",80: "Eighty", 90: "Ninety", 
-    100: "Hundred", 1000: "Thousand", 100000: "Hundred Thousand", 
-    1000000: "Million", 1000000000: "Billion", 1000000000000: "Trillion"
+    100: "Hundred", 1000: "Thousand", 10000: "Thousand", 
+    100000: "Hundred Thousand", 1000000: "Million", 
+    100000000: "Hundred Million", 1000000000: "Billion", 
+    100000000000: "Hundred Billion", 1000000000000: "Trillion",
+    100000000000000: "Hundred Trillion"
 }
 
 def main():
@@ -35,22 +38,32 @@ def search(targetVal):
 
         wholeNum = targetVal // divisor    
         halfNum = (targetVal - (wholeNum * divisor))
+        
+        print(f"wholeNum: {wholeNum}")
 
         thousands:str = typeRes("thousands", targetVal, divisor, wholeNum, halfNum)
         hundreds:str = typeRes("hundreds", targetVal, divisor, wholeNum, halfNum)
         digits:str = typeRes("digits", targetVal, divisor, wholeNum, halfNum)
         tens:str = typeRes("tens", targetVal, divisor, wholeNum, halfNum)
 
-        print(f"wholeNum: {wholeNum}")
-        print(f"halfNum: {halfNum}")
-        print(f"thousandNum: {thousands[0]}")
-        print(f"hundNum: {hundreds[1]}")
-        print(f"tenNum: {tens[0]}")
-        print(f"digits: {digits[0]}")
+        '''if targetVal < 100:
+            print(f"Digits: {digits[0]}, {digits[1]}")
+            print(f"Tens: {tens[0]}, {tens[1]}")
+            result = (cardDict[tens[1]] + numDict[digits[1]])
+        if targetVal > 100 or targetVal < 1000000:
+            result = (numDict[wholeNum] + cardDict[divisor] + cardDict[thousands[0]] + numDict[hundreds[0]] + cardDict[hundreds[1]] + cardDict[tens[0]] + numDict[digits[0]])
+        '''
+       
+        if wholeNum > 19 and wholeNum < 100 or wholeNum < 10:
+            wholeNum = 0
+        if wholeNum >= 100:
+            wholeNum //= 100
 
-        result = (numDict[wholeNum] + cardDict[divisor] + cardDict[thousands[0]] + 
-        numDict[hundreds[0]] + cardDict[hundreds[1]] + cardDict[tens[0]] + numDict[digits[0]])
 
+        print(f"wholeNum2: {wholeNum}")
+        print(f"tens2: {tens[2]}")
+        print(f"digits2: {digits[2]}")
+        result = (numDict[wholeNum] + cardDict[hundreds[2]] + cardDict[tens[2]] + numDict[digits[2]] + cardDict[divisor])
         search_res += (result)
         targetVal = math.floor(targetVal % divisor)
         divisor /= 10
@@ -67,10 +80,16 @@ def typeRes(typeVal: str, targetVal: int, divisor: int, wholeNum: int, halfNum: 
     hundNum = (halfNum // 100) * 100
     hundreds.append(hundNum // (divisor/10)) #Digit calculation for hundreds.
     hundreds.append(round(calcDiv(hundNum))) #Divisor calculation for hundreds. For numbers over 1000
-
+    hundreds.append(round(calcDiv(wholeNum))) # Isolates hundreds from wholeNum 
+    
     tens.append(((targetVal - (wholeNum * divisor) - hundNum) // 10) * 10) # Isolates tens from numbers 100 and over.
+    tens.append(targetVal - (targetVal - (wholeNum * divisor))) #Isolates digits from tens over 20.
+    tens.append(((wholeNum - ((wholeNum // 100) * 100)) // 10) * 10 ) #Isolates tens from wholeNum
 
-    digits.append((targetVal - (wholeNum * divisor) - hundNum) - tens[0]) # Digits calculation
+    digits.append((targetVal - (wholeNum * divisor) - hundNum) - tens[0]) # Digits calculation for large numbers
+    digits.append(targetVal - tens[1]) #Isolates digits from 10s
+    digits.append(wholeNum - ((wholeNum // 100) * 100) - tens[2]) #Isolates digits from wholeNum
+    digits.append(wholeNum - tens[2] - digits[2]) # Isolates tens and digits from wholeNum
 
     match typeVal:
         case "trillions":
@@ -82,14 +101,21 @@ def typeRes(typeVal: str, targetVal: int, divisor: int, wholeNum: int, halfNum: 
         case "thousands":
             return thousands
         case "hundreds":
+            if hundreds[2] < 20:
+                hundreds[2] = 0
             return hundreds
         case "tens":
+            if tens[1] < 20:
+                tens[1] = 0
+            if tens[2] < 20:
+                tens[2] = 0
             return tens
         case "digits":
+            if targetVal < 20:
+                digits[1] = targetVal
             return digits
         case _:
-            return trillions, billions, millions, thousands, hundreds, tens, digits
-
+            return print("No input given.")
 
 def calcDiv(targetVal):
     
@@ -100,10 +126,11 @@ def calcDiv(targetVal):
     match len(str(targetVal)):
         case 5 | 8 | 11 | 14: #Special treatment for ten thousands, millions, billions, and trillions.
             divisor //= 100
-        case 9 | 12 | 15: #Special treatment for hundreds of millions, billions, and trillions.
+        case 6 | 9 | 12 | 15: #Special treatment for hundreds of millions, billions, and trillions.
             divisor //= 1000
         case _:
             divisor //= 10
+    #divisor //= 10
     return divisor
 
 main()
