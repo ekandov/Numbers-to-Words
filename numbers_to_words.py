d55e8aeef9531b5606e0b3c1bfda951352f33487
Ephraim Kandov <efraimkandov@gmail.com>
2024-05-19 12:12:55 -0400

    Updated numbers_to_words.py

--- numbers_to_words.py
+++ numbers_to_words.py
@@ -35,84 +35,53 @@ def search(targetVal):
     print(f"Divisor: {divisor}")
     search_res = ""
     while targetVal:
-
-        wholeNum = targetVal // divisor    
-        halfNum = (targetVal - (wholeNum * divisor))
         
-        print(f"wholeNum: {wholeNum}")
+        thousands:str = typeRes("thousands", targetVal, divisor)
+        hundreds:str = typeRes("hundreds", targetVal, divisor)
+        digits:str = typeRes("digits", targetVal, divisor)
+        tens:str = typeRes("tens", targetVal, divisor)
 
-        thousands:str = typeRes("thousands", targetVal, divisor, wholeNum, halfNum)
-        hundreds:str = typeRes("hundreds", targetVal, divisor, wholeNum, halfNum)
-        digits:str = typeRes("digits", targetVal, divisor, wholeNum, halfNum)
-        tens:str = typeRes("tens", targetVal, divisor, wholeNum, halfNum)
+        print(f"Hundreds0: {hundreds[0]}, Tens0: {tens[0]}, Digits0: {digits[0]}")
 
-        '''if targetVal < 100:
-            print(f"Digits: {digits[0]}, {digits[1]}")
-            print(f"Tens: {tens[0]}, {tens[1]}")
-            result = (cardDict[tens[1]] + numDict[digits[1]])
-        if targetVal > 100 or targetVal < 1000000:
-            result = (numDict[wholeNum] + cardDict[divisor] + cardDict[thousands[0]] + numDict[hundreds[0]] + cardDict[hundreds[1]] + cardDict[tens[0]] + numDict[digits[0]])
-        '''
-       
-        if wholeNum > 19 and wholeNum < 100 or wholeNum < 10:
-            wholeNum = 0
-        if wholeNum >= 100:
-            wholeNum //= 100
-
-
-        print(f"wholeNum2: {wholeNum}")
-        print(f"tens2: {tens[2]}")
-        print(f"digits2: {digits[2]}")
-        result = (numDict[wholeNum] + cardDict[hundreds[2]] + cardDict[tens[2]] + numDict[digits[2]] + cardDict[divisor])
+        result = (numDict[digits[0]] + cardDict[hundreds[0]] + cardDict[tens[0]] + cardDict[divisor])
+        #result += (cardDict[hundreds[3]])
         search_res += (result)
         targetVal = math.floor(targetVal % divisor)
         divisor /= 10
         return search_res
 
-def typeRes(typeVal: str, targetVal: int, divisor: int, wholeNum: int, halfNum: int):
+def typeRes(typeVal: str, targetVal: int, divisor: int):
 
-    trillions, billions, millions, thousands, hundreds, tens, digits = [], [], [], [], [], [], []
+    thousands, hundreds, tens, digits = [], [], [], [] 
 
-    #Needs work - Currently works for numbers up to 19,999
-    thousandNum = (halfNum // 1000) * 1000
-    thousands.append(round(calcDiv(thousandNum))) #Thousands calculation for numbers over 10000
-    
-    hundNum = (halfNum // 100) * 100
-    hundreds.append(hundNum // (divisor/10)) #Digit calculation for hundreds.
-    hundreds.append(round(calcDiv(hundNum))) #Divisor calculation for hundreds. For numbers over 1000
-    hundreds.append(round(calcDiv(wholeNum))) # Isolates hundreds from wholeNum 
-    
-    tens.append(((targetVal - (wholeNum * divisor) - hundNum) // 10) * 10) # Isolates tens from numbers 100 and over.
-    tens.append(targetVal - (targetVal - (wholeNum * divisor))) #Isolates digits from tens over 20.
-    tens.append(((wholeNum - ((wholeNum // 100) * 100)) // 10) * 10 ) #Isolates tens from wholeNum
+    #Calculations that separates numbers before divisor
+    wholeNum = targetVal // divisor
 
-    digits.append((targetVal - (wholeNum * divisor) - hundNum) - tens[0]) # Digits calculation for large numbers
-    digits.append(targetVal - tens[1]) #Isolates digits from 10s
-    digits.append(wholeNum - ((wholeNum // 100) * 100) - tens[2]) #Isolates digits from wholeNum
-    digits.append(wholeNum - tens[2] - digits[2]) # Isolates tens and digits from wholeNum
+    hundreds.append(calcDiv(wholeNum)) #Calculates hundred divisor of wholeNum
+
+    tenNum = (wholeNum // calcDiv(wholeNum)) * calcDiv(wholeNum) #Separates tens from wholeNum 
+    tens.append(tenNum)
+
+    digits.append(wholeNum // calcDiv(wholeNum)) #Separates wholeNum as a single digit from hundreds.
 
     match typeVal:
-        case "trillions":
-            return trillions
-        case "billions":
-            return billions
-        case "millions":
-            return millions
         case "thousands":
             return thousands
         case "hundreds":
-            if hundreds[2] < 20:
-                hundreds[2] = 0
+            if hundreds[0] < 100:
+                hundreds[0] = 0
             return hundreds
         case "tens":
-            if tens[1] < 20:
-                tens[1] = 0
-            if tens[2] < 20:
-                tens[2] = 0
+            if wholeNum > 100:
+                tens[0] = ((wholeNum - tenNum) // 10) * 10
+            if tens[0] < 20:
+                tens[0] = 0
             return tens
         case "digits":
-            if targetVal < 20:
-                digits[1] = targetVal
+            if wholeNum < 100 and wholeNum >= 10:
+                digits[0] = 0
+            if wholeNum < 10:
+                digits[0] = wholeNum
             return digits
         case _:
             return print("No input given.")
@@ -130,7 +99,6 @@ def calcDiv(targetVal):
             divisor //= 1000
         case _:
             divisor //= 10
-    #divisor //= 10
     return divisor
 
 main()
